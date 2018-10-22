70bd7efd567ca4b82a0b2d17a1307c3a84cf4a87
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-22 15:35:26 +0200

    Add Creator to Project

--- Database/dbo/Tables/Project/Project.sql
+++ Database/dbo/Tables/Project/Project.sql
@@ -3,6 +3,7 @@
   [Project_Name] nvarchar(50) NOT NULL,
   [Project_Description] nvarchar(max) NOT NULL,
   [StartDate] datetime2 NOT NULL,
-  [EndDate] datetime2 NOT NULL,
+  [EndDate] datetime2 NULL,
+  [Creator] int FOREIGN KEY REFERENCES [Admin](Employee_Id) NOT NULL,
   PRIMARY KEY ([Project_Id])
 );
