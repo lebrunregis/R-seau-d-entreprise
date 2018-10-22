f8e647403112c827c8c9b99a9aa91351a051407f
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-22 15:54:28 +0200

    Add Empoyee global model

---
+++ Model.Global/Data/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Global.Data
+{
+    class Employee
+    {
+        public int Employee_Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Boolean Actif { get; set; }
+        public string RegNat { get; set; }
+        public string CoordGps { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
