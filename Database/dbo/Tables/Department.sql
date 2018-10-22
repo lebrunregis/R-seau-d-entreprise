d7e8b0bead01411c945762585668a387e8276582
forma1601 <forma1601@FORMAVDI1601>
2018-10-22 12:52:06 +0200

    Corrected nullable fields

--- Database/dbo/Tables/Department.sql
+++ Database/dbo/Tables/Department.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [Department] (
   [Department_Id] int,
-  [Title] nvarchar(50),
-  [Description] nvarchar(MAX),
-  [Admin_Id] int,
+  [Title] nvarchar(50) NOT NULL,
+  [Description] nvarchar(MAX) NOT NULL,
+  [Admin_Id] int NOT NULL,
   PRIMARY KEY ([Department_Id])
 );
 GO
--- Database/dbo/Tables/Document/Document.sql
+++ Database/dbo/Tables/Document/Document.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [Document] (
   [Document_id] int PRIMARY KEY IDENTITY,
-  [Name] nvarchar(max),
-  [Created] datetime,
-  [Link] nvarchar(max),
-  [Size] float,
-  [SHA2] varbinary,
-  [Actif] bit,
-  [Employee_Id] int,
+  [Name] nvarchar(max) NOT NULL,
+  [Created] datetime NOT NULL,
+  [Link] nvarchar(max) NOT NULL,
+  [Size] float NOT NULL,
+  [SHA2] varbinary(32) NOT NULL,
+  [Actif] bit NOT NULL,
+  [Employee_Id] int NOT NULL,
   [NextVersion] int,
 );
 GO
