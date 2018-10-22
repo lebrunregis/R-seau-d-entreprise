e3c3c6fd8fe1daf7de2691881791345a2b57d35c
forma1601 <forma1601@FORMAVDI1601>
2018-10-22 16:54:12 +0200

    Added more global data classes

---
+++ Model.Global/Data/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Global.Data
+{
+    class Document
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public DateTime Created { get; set; }
+        public String Link { get; set; }
+        public float Size { get; set; }
+        public String SHA2 { get; set; }
+        public Boolean Active { get; set; }
+        public int AuthorEmployee { get; set; }
+        public int NextVersion { get; set; }
+    }
+}
