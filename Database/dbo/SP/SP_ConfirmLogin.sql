de1bac58b61bb6246cd81d4b301b97d04879856d
forma1601 <forma1601@FORMAVDI1601>
2018-10-23 10:06:03 +0200

    Added base stored procedures

---
+++ Database/dbo/SP/SP_ConfirmLogin.sql
@@ -0,0 +1,8 @@
+﻿CREATE procedure [dbo].[SP_ConfirmLogin] 
+    @login varchar(50),
+	@pwd varchar(50)
+AS
+BEGIN
+	select count(*) from [Login] where [Login].Login = @login and [Login].Hash = dbo.FN_Hash(@pwd); 
+END
+
---
+++ Database/dbo/SP/SP_Register.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SP_Register]
+	@login varchar(50),
+	@email varchar(50),
+	@pwd varchar(50)
+AS
+BEGIN
+	DECLARE @hash varbinary(32)
+	SET @hash =  [dbo].FN_Hash( @pwd) 
+	INSERT INTO Login (Login,Email,Hash) OUTPUT Inserted.ID VALUES ( @login ,@email,@hash )
+END
