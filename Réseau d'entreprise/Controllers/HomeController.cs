318c117340faa68ddd7435449dc499ea7a39e745
forma1601 <forma1601@FORMAVDI1601>
2018-10-23 14:04:22 +0200

    Auth required -> EmployeeRequired

--- Réseau d'entreprise/Controllers/HomeController.cs
+++ Réseau d'entreprise/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Réseau_d_entreprise.Session.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,7 @@ namespace Réseau_d_entreprise.Controllers
 {
     public class HomeController : Controller
     {
-        [AuthRequired]
+        [EmployeeRequired]
         public ActionResult Index()
         {
             return View();
