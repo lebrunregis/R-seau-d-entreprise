2065c277d74b6eb5f9019eb9c9002485d5b033b9
forma1601 <forma1601@FORMAVDI1601>
2018-10-23 14:00:36 +0200

    Added classes for login

---
+++ Réseau d'entreprise/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using Réseau_d_entreprise.Models.ViewModels;
+using Réseau_d_entreprise.Session;
+using Réseau_d_entreprise.Session.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Réseau_d_entreprise.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Auth
+        [AnonymousRequired]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private ActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+
+        [AnonymousRequired]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AnonymousRequired]
+        [HttpPost]
+        public ActionResult Login(LoginForm form)
+        {
+            int count = Auth.Confirm(form.Login, form.Passwd);
+
+            if (count == 1)
+            {
+                SessionUser.SetSessionUser(new User { Login = form.Login });
+                return RedirectToAction("index", "Home");
+            }
+            return View();
+        }
+
+        [AnonymousRequired]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [AnonymousRequired]
+        [HttpPost]
+        public ActionResult Register(RegistrationForm form)
+        {
+            int count = Auth.Register(form.Login, form.Email, form.Passwd);
+            if (count == 1)
+            {
+                return RedirectToAction("index", "Auth");
+            }
+            return View();
+        }
+    }
+}
