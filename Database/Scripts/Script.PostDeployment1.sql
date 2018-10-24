081f17f3e341eb558b70e4e237a55bc768911f65
forma1601 <forma1601@FORMAVDI1601>
2018-10-24 09:52:13 +0200

    Added demo user stored procedure

--- Database/Scripts/Script.PostDeployment1.sql
+++ Database/Scripts/Script.PostDeployment1.sql
@@ -9,4 +9,15 @@ Modèle de script de post-déploiement
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-EXEC [dbo].SP_Register @LastName = 'Johnny',@FirstName ='Lebrun',@Email ='mon.email1@fa.com',@Password  = 'admin',@RegNat  ='',@Address ='',@Phone = '';
+DELETE FROM Employee
+
+EXEC [dbo].SP_Register_Demo @LastName = 'Aliyah',@FirstName ='Ellis';
+EXEC [dbo].SP_Register_Demo @LastName = 'Izabelle',@FirstName ='Hanna';
+EXEC [dbo].SP_Register_Demo @LastName = 'Tanisha',@FirstName ='Salas';
+EXEC [dbo].SP_Register_Demo @LastName = 'Kerrie',@FirstName ='Dodson';
+EXEC [dbo].SP_Register_Demo @LastName = 'Arabella',@FirstName ='Reyna';
+EXEC [dbo].SP_Register_Demo @LastName = 'Kennedy',@FirstName ='Hutton';
+EXEC [dbo].SP_Register_Demo @LastName = 'Kayley',@FirstName ='Humphries';
+EXEC [dbo].SP_Register_Demo @LastName = 'Katrina',@FirstName ='Boyer';
+EXEC [dbo].SP_Register_Demo @LastName = 'Holly',@FirstName ='Tanner';
+EXEC [dbo].SP_Register_Demo @LastName = 'Kelly',@FirstName ='England';
---
+++ Database/dbo/SP/SP_Register_Demo.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[SP_Register_Demo]
+	@LastName varchar(50),
+	@FirstName varchar(50)
+AS
+BEGIN
+	DECLARE @hash varbinary(32)
+	SET @Hash =  [dbo].FN_Hash( 'admin') 
+	INSERT INTO [dbo].Employee(LastName,FirstName,Email,Passwd,RegNat,Address,Phone) 
+	OUTPUT Inserted.Employee_Id 
+	VALUES ( @LastName , @FirstName,CONCAT(@LastName,@FirstName,'@test.com'),@Hash,CONCAT(@LastName,'-',@FirstName),'','')
+END
