89b7a959096a6c1d3b4ec02a7f2a62336b59fd60
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-30 12:37:19 +0100

    List of employees added

---
+++ ReseauEntreprise/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using D=Model.Global.Data;
+using Model.Global.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReseauEntreprise.Areas.Admin.Models.ViewModels.Employee;
+using Réseau_d_entreprise.Session.Attributes;
+
+namespace ReseauEntreprise.Admin.Controllers
+{
+    [AdminRequired]
+    public class EmployeeController : Controller
+    {
+        // GET: Admin/Employee
+        public ActionResult Index()
+        {
+            IEnumerable<EmployeeList> AllEmployees = EmployeeService.GetAllActive()
+                .Select(e => new EmployeeList()
+                {
+                    LastName = e.LastName,
+                    FirstName = e.FirstName,
+                    Email = e.Email,
+                    Address = e.Address,
+                    Phone = e.Phone,
+                    RegNat = e.RegNat
+                });
+            return View(AllEmployees);
+        }
+    }
+}
---
+++ ReseauEntreprise/Areas/Admin/Models/ViewModels/Employee/EmployeeList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReseauEntreprise.Areas.Admin.Models.ViewModels.Employee
+{
+    public class EmployeeList
+    {
+        [DisplayName("Nom")]
+        public string LastName { get; set; }
+        [DisplayName("Prénom")]
+        public string FirstName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DisplayName("Numéro de registre national")]
+        public string RegNat { get; set; }
+        [DisplayName("Adresse")]
+        public string Address { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("Numéro de téléphone")]
+        public string Phone { get; set; }
+    }
+}
