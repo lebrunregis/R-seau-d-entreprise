667bcbc70bb3a29f9130065f7598d2be5490548b
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-31 14:33:50 +0100

    Added stored procedures for admin

---
+++ Database/dbo/SP/GetAllEmployeesForAdmin.sql
@@ -0,0 +1,4 @@
+﻿CREATE PROCEDURE [dbo].[GetAllEmployeesForAdmin]
+AS
+	SELECT e.Employee_Id, e.FirstName, e.LastName, e.Email, e.RegNat, e.[Address], e.Phone, a.Actif
+	from [dbo].Employee e LEFT JOIN Admin a ON e.Employee_Id=a.Employee_Id WHERE e.Active=1
