242a7522a354807db44a07f95a1ba47aee3c5d28
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-31 10:58:40 +0100

    Recoriged OnDeleteEmployee

--- Database/dbo/Trigger/OnDeleteEmployee.sql
+++ Database/dbo/Trigger/OnDeleteEmployee.sql
@@ -8,5 +8,5 @@ BEGIN
 	--SELECT @EmpId = Employee_Id FROM deleted
 	--INSERT INTO EmployeeStatusHistory(Employee_Id,EmployeeStatus_Id) VALUES (  @EmpId,2)
 	INSERT INTO EmployeeStatusHistory(Employee_Id,EmployeeStatus_Id) SELECT Employee_Id, 2 FROM deleted;
-	UPDATE Employee SET Employee.Active = 0 WHERE EXISTS (SELECT deleted.Employee_Id FROM deleted WHERE Employee.Employee_Id=deleted.Employee_Id);
+	UPDATE Employee SET Employee.Active = 0 WHERE Employee_Id IN (SELECT deleted.Employee_Id FROM deleted);
 END
