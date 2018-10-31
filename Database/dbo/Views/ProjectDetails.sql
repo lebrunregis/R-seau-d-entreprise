e4293931ad07882d3aa9a160f54a0c29406e4cf4
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-31 16:06:03 +0100

    ProjectDetails created

--- Database/dbo/Views/ProjectDetails.sql
+++ Database/dbo/Views/ProjectDetails.sql
@@ -1,11 +1,12 @@
 ﻿CREATE VIEW [dbo].[ProjectDetails]
-	AS SELECT 
-	Project.*,
-	Creator.FirstName + ' ' + Creator.LastName + ' ' + Creator.Email AS CreatorIdentifier,
-	EmployeeTeamLeader.Employee_Id AS ManagerId,
-	Manager.FirstName + ' ' + Manager.LastName + ' ' + Manager.Email AS ManagerIdentifier,
-	EmployeeTeamLeader.date,Manager.* 
-	FROM Project 
-	JOIN Employee AS Creator ON Project.CreatorId = Employee_Id 
-	JOIN EmployeeTeamLeader ON Project.Project_Id = EmployeeTeamLeader.Team_Id 
-	JOIN Employee AS Manager ON EmployeeTeamLeader.Employee_Id = Manager.Employee_Id 
+AS
+	SELECT p.Project_Id, p.Project_Name, p.Project_Description, p.StartDate, p.EndDate, p.CreatorId, e.Employee_Id,
+	    adm.FirstName + ' ' + adm.LastName  + '(' + adm.Email + ')' AS CreatorIdentifier,
+	    manager.FirstName + ' ' + manager.LastName  + '(' + manager.Email + ')' AS ManagerIdentifier
+    FROM Project p
+	INNER JOIN EmployeeProjectManager e ON p.Project_Id=e.Project_Id
+    INNER JOIN 
+        (SELECT max(e2.[Date]) maxDate, e2.Project_Id FROM EmployeeProjectManager e2 GROUP BY e2.Project_Id) e3
+        ON e.[Date]=e3.maxDate AND e.Project_Id=e3.Project_Id
+	INNER JOIN Employee adm ON p.CreatorId=adm.Employee_Id
+	INNER JOIN Employee manager ON e.Employee_Id=manager.Employee_Id
