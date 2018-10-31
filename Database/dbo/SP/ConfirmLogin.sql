9ea74e26a11e36e6a6c2c0da3c1f12cef6041899
forma1601 <forma1601@FORMAVDI1601>
2018-10-31 11:13:24 +0100

    corrected merge duplicate and data type error in confirmlogin

--- Database/dbo/SP/ConfirmLogin.sql
+++ Database/dbo/SP/ConfirmLogin.sql
@@ -1,8 +1,8 @@
 ﻿CREATE procedure [dbo].[ConfirmLogin] 
-    @Email varchar(360),
-	@Password varchar(50)
+    @Email nvarchar(360),
+	@Password nvarchar(50)
 AS
 BEGIN
-	select Employee_Id from [dbo].[Employee] where [dbo].[Employee].Email = @Email and [dbo].[Employee].Passwd = [dbo].FN_Hash(@Password) and [dbo].[Employee].Active = 1; 
+	return select Employee_Id from [dbo].[Employee] where [dbo].[Employee].Email = @Email and [dbo].[Employee].Passwd = [dbo].FN_Hash(@Password) and [dbo].[Employee].Active = 1; 
 END
 
