0ad5f99c42ea2798d713e51426cb2fafd546da0a
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-10-31 10:53:09 +0100

    Changed trigger to multiple deletions

--- Database/dbo/Trigger/OnDeleteEmployee.sql
+++ Database/dbo/Trigger/OnDeleteEmployee.sql
@@ -4,8 +4,9 @@ INSTEAD OF DELETE
 AS
 BEGIN
 	SET NOCOUNT ON
-		DECLARE @EmpId int
-	SELECT @EmpId = Employee_Id FROM deleted
-	INSERT INTO EmployeeStatusHistory(Employee_Id,EmployeeStatus_Id) VALUES (  @EmpId,2)
-	UPDATE Employee SET Active = 0 WHERE Employee_Id = @EmpId;
+		--DECLARE @EmpId int
+	--SELECT @EmpId = Employee_Id FROM deleted
+	--INSERT INTO EmployeeStatusHistory(Employee_Id,EmployeeStatus_Id) VALUES (  @EmpId,2)
+	INSERT INTO EmployeeStatusHistory(Employee_Id,EmployeeStatus_Id) SELECT Employee_Id, 2 FROM deleted;
+	UPDATE Employee SET Employee.Active = 0 WHERE EXISTS (SELECT deleted.Employee_Id FROM deleted WHERE Employee.Employee_Id=deleted.Employee_Id);
 END
