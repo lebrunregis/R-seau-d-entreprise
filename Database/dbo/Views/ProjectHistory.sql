037ae68b4d9e02ae6781df9009ec142ee62bcdc1
forma1601 <forma1601@FORMAVDI1601>
2018-10-31 16:14:15 +0100

    Added ProjectHistory View

---
+++ Database/dbo/Views/ProjectHistory.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[ProjectHistory]
+	AS SELECT 
+	Project.Project_Id,
+	Project.Project_Description,
+	Project.StartDate AS ProjectStartDate,
+	Project.EndDate AS ProjectEndDate,
+	Project.CreatorId,
+	Creator.FirstName + ' ' + Creator.LastName + ' (' + Creator.Email + ')' AS CreatorIdentifier,
+	EmployeeTeamLeader.Employee_Id AS TeamLeaderId,
+	Manager.FirstName + ' ' + Manager.LastName + ' (' + Manager.Email + ')' AS TeamLeaderIdentifier,
+	EmployeeTeamLeader.date AS ManagerStartDate,
+	Team.Team_Name,
+	TeamProject.StartDate AS TeamStartDate,
+	TeamProject.EndDate AS TeamEndDate
+	FROM Project 
+	JOIN Employee AS Creator ON Project.CreatorId = Employee_Id 
+	JOIN EmployeeTeamLeader ON Project.Project_Id = EmployeeTeamLeader.Team_Id 
+	JOIN Employee AS Manager ON EmployeeTeamLeader.Employee_Id = Manager.Employee_Id
+	JOIN TeamProject ON TeamProject.Team_Id = Project.Project_Id
+	JOIN Team ON Team.Team_Id = TeamProject.Team_Id
+	
