f0d328a8d6215b2fe7ef6c7ce28829ab80b651b1
forma1601 <forma1601@FORMAVDI1601>
2018-11-02 16:29:24 +0100

    Sql fixes

--- Database/dbo/SP/Project/CreateProject.sql
+++ Database/dbo/SP/Project/CreateProject.sql
@@ -14,7 +14,7 @@ IF EXISTS(SELECT * FROM [dbo].[Admin] WHERE Employee_Id = @creator AND Actif = 1
        BEGIN
            INSERT INTO [dbo].Project (Project_Name, Project_Description, [CreatorId]) VALUES (@name, @description, @creator);
 	       SET @project_id = convert(int,SCOPE_IDENTITY() );
-	       INSERT INTO [dbo].EmployeeProjectManager(Project_Id, Employee_Id) VALUES (@project_id, @project_manager);
+	       INSERT INTO [dbo].ProjectManager(Project_Id, Employee_Id) VALUES (@project_id, @project_manager);
 	       SELECT @project_id
        END
 END
--- Database/dbo/Tables/Project/ProjectManager.sql
+++ Database/dbo/Tables/Project/ProjectManager.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE [dbo].[EmployeeProjectManager]
+﻿CREATE TABLE [dbo].[ProjectManager]
 (
   [Employee_Id] int NOT NULL FOREIGN KEY REFERENCES Employee(Employee_Id),
   [Project_Id] int NOT NULL FOREIGN KEY REFERENCES Project(Project_Id),
   [Date] datetime2(7) DEFAULT GetDate(),
   PRIMARY KEY ([Date], [Project_Id]),
-  CONSTRAINT FK_ProjectManagerEmployeeId FOREIGN KEY (Employee_Id) REFERENCES Employee(Employee_Id),
+  CONSTRAINT FK_ProjectManagerId FOREIGN KEY (Employee_Id) REFERENCES Employee(Employee_Id),
   CONSTRAINT FK_ProjectManagerProjectId FOREIGN KEY(Project_Id) REFERENCES Project(Project_Id)
 )
