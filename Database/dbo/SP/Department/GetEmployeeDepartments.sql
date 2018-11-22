1c3b3ffb67e2ff36f57c41f1ad5e92acfdf54e81
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-11-22 14:11:40 +0100

    Correction GetEmployeeDepartment

--- Database/dbo/SP/Department/GetEmployeeDepartments.sql
+++ Database/dbo/SP/Department/GetEmployeeDepartments.sql
@@ -2,6 +2,6 @@
 	@EmployeeId int
 AS
 	SELECT EmployeeDepartment.Department_Id , Name , Description, StartDate,EndDate,Created,Creator_Id,Active FROM EmployeeDepartment 
-	JOIN Department ON EmployeeDepartment.Department_Id = EmployeeDepartment.Department_Id 
-	WHERE Employee_Id = @EmployeeId AND EndDate IS NULL
+	JOIN Department ON Department.Department_Id = EmployeeDepartment.Department_Id 
+	WHERE Employee_Id = @EmployeeId AND EndDate IS NULL AND Department.Active=1
 	
