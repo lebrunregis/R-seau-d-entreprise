89a395d5a06d42da2bb21b3e66f691952da9ca85
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-11-28 17:12:24 +0100

    messages

---
+++ ReseauEntreprise/Areas/Employee/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using ReseauEntreprise.Areas.Employee.Models.ViewModels.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReseauEntreprise.Areas.Employee.Controllers
+{
+    public class MessageController : Controller
+    {
+        public ActionResult Compose()
+        {
+            SendForm form = new SendForm();
+            return PartialView(form);
+        }
+        [HttpPost]
+        public ActionResult Send(SendForm form)
+        {
+            return View();
+        }
+        public ActionResult Visualise(int id)
+        {
+
+        }
+    }
+}
