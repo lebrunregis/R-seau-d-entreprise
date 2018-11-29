2c6d786c6fbbb3a7c3a3f91b04dea7acac628366
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-11-29 15:47:29 +0100

    Added constructor without parameter to message

--- Model.Client/Data/Message.cs
+++ Model.Client/Data/Message.cs
@@ -15,6 +15,10 @@ namespace Model.Client.Data
         private int author;
         private int? parent;
 
+        public Message()
+        {
+        }
+
         public Message(int? id, string title, DateTime created, string body, int author, int? parent)
         {
             Id = id;
