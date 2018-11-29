3c10db7280462f514dbc4b653b969946e56cea2f
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-11-29 09:06:02 +0100

    message viewform

--- ReseauEntreprise/Areas/Employee/Models/ViewModels/Message/ViewForm.cs
+++ ReseauEntreprise/Areas/Employee/Models/ViewModels/Message/ViewForm.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using C = Model.Client.Data;
 
 namespace ReseauEntreprise.Areas.Employee.Models.ViewModels.Message
 {
     public class ViewForm
     {
         public int Id { get; set; }
-
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public int? Parent { get; set; }
+        public C.Employee Author { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreationTime { get; set; }
     }
 }
