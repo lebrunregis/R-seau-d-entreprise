b25f79e00ab3d71ce8f1087f0b92a0246201d012
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-12-06 14:58:12 +0100

    tested signalr

---
+++ ReseauEntreprise/Areas/Employee/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Réseau_d_entreprise.Session;
+using Réseau_d_entreprise.Session.Attributes;
+using SignalRChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReseauEntreprise.Areas.Employee.Controllers
+{
+    [RouteArea("Employee")]
+    [EmployeeRequired]
+    public class TestController : Controller
+    {
+        // GET: Employee/Test
+
+        public ActionResult Index()
+        {
+            int MyId = SessionUser.GetUser().Id;
+            //ChatHub.SetUserId(MyId);
+            return View();
+        }
+    }
+}
