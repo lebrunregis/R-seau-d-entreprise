0dea08d36baba312a8228b55065b35666540a2ee
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-12-07 13:30:40 +0100

    test controller

---
+++ ReseauEntreprise/Areas/Employee/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReseauEntreprise.Areas.Employee.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Employee/Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
