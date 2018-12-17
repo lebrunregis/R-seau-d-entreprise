ff989577a1201c513e5925aa707414edcfa0ce98
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2018-12-17 17:10:30 +0100

    filestream wip

---
+++ Database/dbo/FN/FN_Hash.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [dbo].[FN_Hash]
+(
+	@pwd NVARCHAR(50)
+)
+RETURNS varbinary(32)
+AS 
+BEGIN
+RETURN (HASHBYTES('SHA2_256' , CONCAT('luf§gèy(g!èouhgfèo(yiè123456hg875gyTR§5TT§§5555§f', @pwd, 'ITFvgt6(è§(HGç678(hGIYF§t976Gç§yoOGo!è§((96TygUHG7§!è§7UYOYGO')))
+END
