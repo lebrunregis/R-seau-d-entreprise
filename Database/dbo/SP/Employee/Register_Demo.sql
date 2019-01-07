4a3b0fc0edaf67ac99f3f486d03840879efae421
forma1601 <forma1601@FORMAVDI1601>
2019-01-07 10:58:04 +0100

    filling national numbers and phone numbers

--- Database/dbo/SP/Employee/Register_Demo.sql
+++ Database/dbo/SP/Employee/Register_Demo.sql
@@ -7,7 +7,7 @@ BEGIN
 	DECLARE @hash varbinary(32);
 	SET @Hash =  [dbo].FN_Hash( 'admin') ;
 	INSERT INTO [dbo].Employee(LastName,FirstName,Email,RegNat,Passwd,Address,Phone)
-	VALUES ( @LastName , @FirstName,'','', @Hash,'Gosselies Belgique',Null);
-	UPDATE [dbo].Employee SET Email=CONCAT(convert(varchar(10),SCOPE_IDENTITY()), '@test.be'), RegNat = convert(varchar(10),SCOPE_IDENTITY()) WHERE Employee_Id = convert(int,SCOPE_IDENTITY());
+	VALUES ( @LastName , @FirstName,'','', @Hash,'Gosselies Belgique',convert(VARCHAR(50),convert(bigint,RAND()*10000000000,0)));
+	UPDATE [dbo].Employee SET Email=CONCAT(convert(varchar(10),SCOPE_IDENTITY()), '@test.be'), RegNat = convert(varchar(10),convert(bigint,RAND(SCOPE_IDENTITY())*10000000000,0)) WHERE Employee_Id = CONVERT(int,SCOPE_IDENTITY());
   COMMIT TRANSACTION
 END
