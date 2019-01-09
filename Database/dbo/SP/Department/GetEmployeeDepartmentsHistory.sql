d0e3c60e4bdc9931de4816d7fff035ecfa63a56c
Ekaterina Moskovkina <katerina.moskovkina@yahoo.com>
2019-01-09 11:38:43 +0100

    fix in GetEmployeeDepartmentHistory

--- Database/dbo/SP/Department/GetEmployeeDepartmentsHistory.sql
+++ Database/dbo/SP/Department/GetEmployeeDepartmentsHistory.sql
@@ -2,6 +2,6 @@
 	@EmployeeId int
 AS
 	SELECT Id,EmployeeDepartment.Department_Id AS Department_Id, StartDate,EndDate ,Department.Name FROM EmployeeDepartment 
-	JOIN Department ON EmployeeDepartment.Department_Id = EmployeeDepartment.Department_Id 
+	JOIN Department ON EmployeeDepartment.Department_Id = Department.Department_Id 
 	WHERE Employee_Id = @EmployeeId
 	
